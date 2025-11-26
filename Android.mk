0224cffeae358ad5f7d4fd56ca73554d5c1984af
Nick Reuter <nreuter85@gmail.com>
2025-11-26 15:11:51 -0600

    avatrn: Initial blobs from avatrn_g-user 15 V1UANS35H.98-24-1
    Change-Id: I87912ccfb60c30a22b5689ae694c8a49d0ac9e85

---
+++ Android.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),avatrn)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,fb1fcc6551bfc8062e6b373619556f5d065e7a50)
+$(call add-radio-file-sha1-checked,radio/aop.img,2588600bdb12b92f2f1ad0a1a50d30c47d1b8f86)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,5168aad31f7ba7cdfd9a16fb96f60e445936a5f4)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,5c616a71fb2b342a8dca0b30b7861433c267413f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,45fd611e33f0ff555bc4e79e42f3261e6340e188)
+$(call add-radio-file-sha1-checked,radio/dsp.img,27c1b1cec2e9bbd185ab36eba01de3c519570a5d)
+$(call add-radio-file-sha1-checked,radio/fsg.img,3e1b5db412709c0c84d05f227a8fb0b84571e84f)
+$(call add-radio-file-sha1-checked,radio/hyp.img,406bce42dff518a97a76dd5f755520e8f9a09e22)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,ae71198285ee874b1ee55294b9340bb165709855)
+$(call add-radio-file-sha1-checked,radio/logo.img,e9ba25b5f0a2e06775348197627dfcb2fa5ad72f)
+$(call add-radio-file-sha1-checked,radio/modem.img,35a29a33537332d5fceae2f4d6bcb6668dc56639)
+$(call add-radio-file-sha1-checked,radio/prov.img,9e852a1875969392d2083b4992edc4da0508cfa0)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,98d34658cecb0327c59d9d50ca7cc5e355dd5be6)
+$(call add-radio-file-sha1-checked,radio/shrm.img,89a1610f107c1e5c1a711ecc5c32609816471e95)
+$(call add-radio-file-sha1-checked,radio/storsec.img,ba9b3e070edb54e998222d112ffd569142803d7d)
+$(call add-radio-file-sha1-checked,radio/tz.img,c1ad1204d6a18f51eab159d8c6e843a008a0583d)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,3d5296d598a8593a13f8fe4d4324038380afc9eb)
+$(call add-radio-file-sha1-checked,radio/xbl.img,06d8f3638d6f725586e09e759569ba6008f056be)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,0137f6c0f0aff256fa11849f49d4ea8c6eddcf0b)
+
+endif
