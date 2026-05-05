1b860693e898b30e2a39a7066afa6abfde5a085d
Nick Reuter <nreuter85@gmail.com>
2026-05-05 00:43:55 -0500

    avatrn: Add missing proprietary firmware
    Change-Id: I6512086df18453ee0509dd7b45fa16fda2b307d5

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ ifeq ($(TARGET_DEVICE),avatrn)
 
 $(call add-radio-file-sha1-checked,radio/abl.img,7f636fbd1cdc04d0beba6d88e201975284880ff9)
 $(call add-radio-file-sha1-checked,radio/aop.img,70c00bbf776d3500161bc5f14fcd5ae849d39af6)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,89e7875ec34e6c0e43d4be2582605f12a0df154e)
 $(call add-radio-file-sha1-checked,radio/bluetooth.img,2defc3e349ad6f0825efd0655594673577295e0c)
 $(call add-radio-file-sha1-checked,radio/cpucp.img,be9defdf8fb7c374087f2a4ac86bfd6f60755baf)
 $(call add-radio-file-sha1-checked,radio/devcfg.img,866aeaa8f225c3d8a10485310c64ef3f3e370015)
@@ -22,8 +23,10 @@ $(call add-radio-file-sha1-checked,radio/qupfw.img,3bea80fab12f6db4415034c74c5f3
 $(call add-radio-file-sha1-checked,radio/shrm.img,293f9638ad795da18dad2a7b9efd0af4278cb71b)
 $(call add-radio-file-sha1-checked,radio/storsec.img,85e110315aee59ecb4edc41e9fe0c3f0ca5a2b24)
 $(call add-radio-file-sha1-checked,radio/tz.img,f3f727a9ad412956c9ee16d9926c147255f840b0)
+$(call add-radio-file-sha1-checked,radio/uefi.img,419057c5e705a3a857946aed8f92f64142a0253d)
 $(call add-radio-file-sha1-checked,radio/uefisecapp.img,584392c786583204c529ebb1f63b2b1ccf33b998)
 $(call add-radio-file-sha1-checked,radio/xbl.img,cce3ec174245d68eb3719350776e3ac5215eedac)
 $(call add-radio-file-sha1-checked,radio/xbl_config.img,b43275ff9b139e66b82714785cfabb02f4b54a7c)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,b1879440f92d2a8aa783f06fe9f8d2f356add926)
 
 endif
